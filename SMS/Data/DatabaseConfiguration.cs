06efb3424c48af1f380b78e66d271a5720c1a1b3
Kay <77486531+HelloItsKay@users.noreply.github.com>
2022-03-21 19:20:41 +0200

    Upload

---
+++ SMS/Data/DatabaseConfiguration.cs
@@ -0,0 +1,8 @@
+﻿namespace SMS.Data
+{
+    public class DatabaseConfiguration
+    {
+        public const string ConnectionString =
+            @"Server=DESKTOP-E7OI1CT\SQLEXPRESS;Database=SMS;Trusted_Connection=True;Integrated Security=True;";
+    }
+}
